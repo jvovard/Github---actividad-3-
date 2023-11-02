f09fdb75b494b8b69471c8898d5b349f4ff333b7
Joaquin Vovard Barrientos <141941552+jvovard@users.noreply.github.com>
2023-11-02 12:11:08 -0300

    rama js

---
+++ pepe/js.js
@@ -0,0 +1,12 @@
+
+document.querySelector('#about').addEventListener('click', function() {
+    alert('¡Haz clic en la sección About!');
+});
+
+document.querySelector('#projects').addEventListener('click', function() {
+    alert('¡Haz clic en la sección Projects!');
+});
+
+document.querySelector('#contact').addEventListener('click', function() {
+    alert('¡Haz clic en la sección Contact!');
+});
